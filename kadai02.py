7c5d428f52ed8944fb9674c070649d70976ed10d
k018c1082 <k018c1082@it-neec.jp>
2020-07-06 15:29:40 +0900

    create

--- kadai02.py
+++ kadai02.py
@@ -1,11 +1,14 @@
+import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 
-def hello_world():
-    return 'Hello World!'
+def hiniti():
+    dt = datetime.datetime.now()    
+    return dt.strftime('%m/%d %H:%M')
     
 if __name__=='__main__':
     app.debug = True
