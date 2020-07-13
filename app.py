3155983f829ccf17f64f2f6bc71e71ad8a983168
k018c1082 <k018c1082@it-neec.jp>
2020-07-13 13:29:20 +0900

    3.5template(for)

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('index.html', num = 7)
+    return render_template('index.html', list=["アイテム１","アイテム2","アイテム3"])
     
 if __name__=='__main__':
     app.debug = True
