f0ea79e1aac2b0ef1ca4cf0ee8a486675de75756
k018c1082 <k018c1082@it-neec.jp>
2020-07-14 17:42:18 +0900

    4.3.routing

---
+++ kadai03_1.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html', num = 10)
+    
+if __name__=='__main__':
+    app.debug = True
+    app.run()
